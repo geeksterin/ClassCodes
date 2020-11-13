772802bf6be8d951e9110f8a0bf59379893cbd40
bittoo aggarwal <bitz1119@gmail.com>
2020-11-13 14:14:08 +0530

    November 12

---
+++ src/November10/TestDiscussion.java
@@ -0,0 +1,16 @@
+package November10;
+
+public class TestDiscussion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 4;
+		int y = 3;
+		System.out.println(x^y);
+		System.out.println(x&y);
+		System.out.println(x|y);
+		
+		
+	}
+
+}
---
+++ src/November12/HistogramArea.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int largestRectangleArea(int[] h) {
+        Stack<Integer> st=new Stack<>();
+        st.push(-1);
+        int maximum_area=0;
+        // 2,5,5,5,4,4,3,5
+        for(int i=0;i<h.length;i++){
+            if(st.peek()==-1||h[st.peek()]<h[i]){
+                st.push(i);
+            }else{
+                while(st.peek()!=-1&&h[st.peek()]>=h[i]){
+                   
+                    int height=h[st.pop()];
+                    int area=height*(i-st.peek()-1);
+                    maximum_area=Math.max(area,ma);
+                }
+                st.push(i);
+            }
+        }
+        while(st.peek()!=-1){
+             int height=h[st.pop()];
+            int area=height*(h.length-st.peek()-1);
+            ma=Math.max(area,ma);
+           
+        }
+        return ma;
+       
+    }
+}
