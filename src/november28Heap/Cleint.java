0da63990a47250759b522dff5c0a60180f862a6c
bittoo aggarwal <bitz1119@gmail.com>
2020-11-28 13:04:00 +0530

    Heap

---
+++ src/november28Heap/Cleint.java
@@ -0,0 +1,14 @@
+package november28Heap;
+
+public class Cleint {
+
+    public static void main(String[] args){
+        Heap h = new Heap();
+        h.add(10);
+        h.add(30);
+        h.add(9);
+        h.add(15);
+        h.add(40);
+        h.display();
+    }
+}
