cf4c4ba8b96a880447c2a5107da58ea8ac10ced3
bittoo aggarwal <bitz1119@gmail.com>
2020-12-06 21:50:48 +0530

    01 Knapsack dp variation

--- src/december05/DP.java
+++ src/december05/DP.java
@@ -74,32 +74,123 @@ public class DP {
     }
 
     // implement memoization and tabulation
-    public static int countPath(int x,int y){
+    public static int countPath(int x,int y,int dp[][]){
         if(x==0 && y ==0){
             return 1;
         }
         else if(x < 0 || y < 0){
             return 0;
         }
+        if(dp[x][y] != 0){
+            return dp[x][y];
+        }
         else{
-            return countPath(x-1,y) + countPath(x,y-1);
+            dp[x][y] = countPath(x-1,y,dp) + countPath(x,y-1,dp);
+            return dp[x][y];
         }
 
     }
 
-    public static void main(String[] args){git st
-//        int[] dp = new int[100];
-//        System.out.print(staircase(5,dp) + " ");
-//        System.out.print(staircaseIter(5));
+    public static int countPathIter(int x,int y){
+        int[][] dp = new int[x][y];
+        for (int i=0;i<x;i++){
+            dp[i][0] = 1;
+        }
+        for (int j = 0;j<y;j++){
+            dp[0][j] = 1;
+        }
 
-        int houses[] = {6,7,1,3,8,2,4};
-        int dp[] = new int[100];
-        int n = houses.length;
-        System.out.print(maxLootIter(houses));
+        for (int i=1;i<x;i++){
+            for (int j = 1;j<y;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[x-1][y-1];
 
     }
 
 
+    public static int knapSack01(int[] weight,int[] value,int capacity,int index,int[][] dp){
 
+        //capacity is zero
+        if(capacity == 0){
+            return 0;
+        }
+        // index == length
+        if(index >= weight.length) {
+            return 0;
+        }
+        // capacity < weight
+
+        if(dp[capacity][index] != -1){
+            return dp[capacity][index];
+        }
+
+        if(capacity < weight[index]){
+            int ans = knapSack01(weight,value,capacity,index+1,dp);
+            dp[capacity][index] = ans;
+            return ans;
+        }
+
+        // scenario include me
+        int ans1 = knapSack01(weight,value,capacity-weight[index],index+1,dp) + value[index];
+
+        // scenaio do not include me
+        int ans2 = knapSack01(weight,value,capacity,index+1,dp);
+
+        // my ans
+        int ans = Math.max(ans1,ans2);
+        dp[capacity][index]  = ans;
+        return ans;
+    }
+
+    public static int knapSack01Iter(int[] weight,int[] value,int capacity){
+        int[][] dp = new int[capacity+1][weight.length+1];
+
+
+        for (int i = 1;i<capacity+1;i++){
+            for (int j = 1;j<weight.length+1;j++){
+                // can i include myself
+                if(i >= weight[j-1]){
+                    dp[i][j] = Math.max(value[j-1]+dp[i-weight[j-1]][j-1],dp[i][j-1]);
+                }
+                else{
+                    dp[i][j] = dp[i][j-1];
+                }
+
+            }
+        }
+
+        return dp[capacity][weight.length];
+
+
+    }
+
+    public static void main(String[] args){
+//        int[] dp = new int[100];
+//        System.out.print(staircase(5,dp) + " ");
+//        System.out.print(staircaseIter(5));
+//        int houses[] = {6,7,1,3,8,2,4};
+//        int dp[] = new int[100];
+//        int n = houses.length;
+//        System.out.print(maxLootIter(houses));
+
+//        int[][] dp = new int[100][100];
+//        System.out.println( countPath(2,2,dp));
+
+        int[] weight = {2,3,1,4};
+        int[] value = {4,5,3,7};
+        int capacity = 5;
+        int[][] dp = new int[capacity+5][weight.length + 5];
+        for (int i=0;i<capacity+5;i++){
+            for (int j = 0;j<weight.length+5;j++){
+                dp[i][j] =  -1;
+            }
+        }
+
+//        System.out.print(knapSack01(weight,value,capacity,0,dp));
+        System.out.print(knapSack01Iter(weight,value,capacity));
+    }
 
 }
